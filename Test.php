871dd28eb9f3ec5b9b19b330656e3109e96439d7
Daniil <daniilfgs@gmail.com>
2021-01-26 20:43:43 +0400

    First comit

---
+++ Test.php
@@ -0,0 +1,86 @@
+<?php
+$hm = 'localhost';
+$db = 'users';
+$un = 'root';
+$pw = 'root';
+
+$conn = new mysqli($hm, $un, $pw, $db);
+if ($conn->connect_error) die($conn->connect_error);
+
+if (isset($_POST['reg'])) {
+    registr($conn);
+}
+if (isset($_POST['enter'])){
+    enter_user($conn);
+}
+
+function registr($conn)
+{
+    if (isset($_POST['name']) && isset($_POST['login']) && isset($_POST['pass'])) {
+        $name = $_POST['name'];
+        $login = $_POST['login'];
+        $pass = $_POST['pass'];
+        $query_defence = "SELECT name FROM user WHERE name='$name' and login = '$login' and password ='$pass'";
+        $res = $conn->query($query_defence);
+        $row_cnt = mysqli_num_rows($res);
+        if ($row_cnt > 0) {
+            echo "Такой пользователь уже есть";
+        } else {
+            $query = "insert into user (name,login,password) values" . "('$name','$login','$pass')";
+            $result = $conn->query($query);
+            if (!$result) {
+                echo "DONT WORK  $query";
+            } else {
+                echo "WORK";
+            }
+        }
+    }
+}
+
+function enter_user($conn)
+{
+    if (isset($_POST['login_enter']) && isset($_POST['pass_enter'])) {
+        $login = $_POST['login_enter'];
+        $pass = $_POST['pass_enter'];
+        $query_defence = "SELECT name FROM user WHERE  login = '$login' and password ='$pass'";
+        $res = $conn->query($query_defence);
+        $row_cnt = mysqli_num_rows($res);
+        if ($row_cnt > 0) {
+            echo "Добро пожаловать ";
+        } else {
+            echo "Неверный логин или пароль";
+        }
+    }
+}
+
+
+echo <<<END
+
+
+    <form method="post" >
+
+        Ваше имя ?<input type="text" name="name" placeholder="Max" required="required" value="null"><br>
+        Логин <input type="text" name="login" placeholder="Max" required="required" value="null"><br>
+        Пароль<input type="password" name="pass" placeholder="Max"   ><br>
+
+        <input type="submit" value="регистрация" name="reg">
+    <form/>
+
+
+
+END;
+
+
+echo <<<END
+<hr>  
+         <form action="">
+            Логин<input type="text" placeholder="введите логин" name="login_enter" >
+            Пароль<input type="text" placeholder="введите пароль" name="pass_enter" >
+            
+            <input type="submit" value="Войти" name="enter">
+            
+         </form>
+
+END;
+
+
