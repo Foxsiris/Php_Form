db3d5bef2918a3900fc3c435e9695758a313b3f4
Daniil <daniilfgs@gmail.com>
2021-02-01 16:15:50 +0400

    Изучил Миксины,Переменные импорты Scss

--- Test.php
+++ Test.php
@@ -3,8 +3,19 @@ $hm = 'localhost';
 $db = 'users';
 $un = 'root';
 $pw = 'root';
-
 $conn = new mysqli($hm, $un, $pw, $db);
+
+$login = $_POST['login_enter'] ;
+$pass = $_POST['pass_enter'];
+if ($login && $pass != ''){
+    setcookie('login',$login,0,'/');
+    setcookie('password',$pass,0,'/');
+
+}
+$id = get_id($conn,$_COOKIE['login'],$_COOKIE['password']);
+echo $id;
+
+
 if ($conn->connect_error) die($conn->connect_error);
 
 if (isset($_POST['reg'])) {
@@ -13,6 +24,24 @@ if (isset($_POST['reg'])) {
 if (isset($_POST['enter'])) {
     enter_user($conn);
 }
+
+function enter_user($conn)
+{
+    if (isset($_POST['login_enter']) && isset($_POST['pass_enter'])) {
+        $login = $_POST['login_enter'];
+        $pass = $_POST['pass_enter'];
+        $query_defence = "SELECT name FROM user WHERE  login = '$login' and password ='$pass'";
+        $res = $conn->query($query_defence);
+        $row_cnt = mysqli_num_rows($res);
+        if ($row_cnt > 0) {
+            echo "Добро пожаловать ";
+            echo $login,$pass;
+        } else {
+            echo "Неверный логин или пароль";
+        }
+    }
+}
+
 
 function registr($conn)
 {
@@ -34,24 +63,11 @@ function registr($conn)
                 echo "WORK";
             }
         }
+
     }
 }
 
-function enter_user($conn)
-{
-    if (isset($_POST['login_enter']) && isset($_POST['pass_enter'])) {
-        $login = $_POST['login_enter'];
-        $pass = $_POST['pass_enter'];
-        $query_defence = "SELECT name FROM user WHERE  login = '$login' and password ='$pass'";
-        $res = $conn->query($query_defence);
-        $row_cnt = mysqli_num_rows($res);
-        if ($row_cnt > 0) {
-            echo "Добро пожаловать ";
-        } else {
-            echo "Неверный логин или пароль";
-        }
-    }
-}
+
 
 
 echo <<<END
@@ -94,37 +110,47 @@ echo <<<END
 
 END;
 
+
 if (isset($_POST['add_do'])) {
-    add_do($conn);
+    add_do($conn,$id);
 }
 
-function add_do($conn)
+
+function add_do($conn,$id)
 {
-    if (isset($_POST['ToDo']) && $_POST['ToDo'] != "null") {
-        $todo = $_POST['ToDo'];
-        $query = "insert into todo (name,user_id) values" . "('$todo',16)";
-        $result = $conn->query($query);
-        if (!$result) {
-            echo "DONT WORK  $query";
-        } else {
-            echo "WORK";
-            $q = "SELECT todo.name FROM `user` INNER JOIN todo on user.id = todo.user_id";
-            $r = $conn->query($q);
-            $rows = mysqli_num_rows($r); // количество полученных строк
-            for ($i = 0; $i < $rows; ++$i) {
-                $row = mysqli_fetch_row($r);
-                echo "<li>";
-                for ($j = 0; $j < 1; ++$j) {
-                    echo "<td>$row[$j]</td>";
+    if (isset($_POST['add_do'])) {
+                $login = $_POST['login_enter'];
+                $pass = $_POST['pass_enter'];
+                $todo = $_POST['ToDo'];
+                $query = "insert into todo (name,user_id) values" . "('$todo',$id)";
+                $result = $conn->query($query);
+                if (!$result) {
+                    echo "DONT WORK  $query";
+                } else {
+                    echo "WORK";
+                    $q = "SELECT * FROM `user` INNER JOIN todo on user.id = todo.user_id";
+                    $r = $conn->query($q);
+                    $rows = mysqli_num_rows($r); // количество полученных строк
+                    for ($i = 0; $i < $rows; ++$i) {
+                        $row = mysqli_fetch_row($r);
+                        echo "<li>";
+                        for ($j = 0; $j < 1; ++$j) {
+                            echo $row[$j];
+                        }
+                        echo "</li>";
+                    }
+                    mysqli_free_result($r);
                 }
-                echo "</li>";
             }
-            mysqli_free_result($r);
-
-        }
-
-    }
 }
+
+function get_id($conn,$login,$pass){
+    $qq = "SELECT * FROM `user` WHERE login ='$login' and password ='$pass' ";
+    $rr = $conn->query($qq);
+    $roww = mysqli_fetch_row($rr);
+    return (int)$roww[0];
+}
+
 
 
 
